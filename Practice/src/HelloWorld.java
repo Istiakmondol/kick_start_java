79ed99920134e711c78e9a552e2f24459d9c323f
Istiakmondol <theistiakmondol@gmail.com>
2026-08-16 21:40:10 +0600

    16-08-2026

---
+++ Practice/src/HelloWorld.java
@@ -0,0 +1,14 @@
+//Program that will print “Hello World”
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //Program that will print “Hello World”
+        System.out.print("Hello World");
+    }
+}
+
+class NewLine {
+    public static void main(String[] args) {
+        System.out.println("Hello World \nThis is my first program. \nC is fun");
+    }
+}
